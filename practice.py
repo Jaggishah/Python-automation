678321185f135f339f1311f69d74bb355b23cc93
Jaggishah <jugdeepshah@gmail.com>
2022-10-02 00:10:04 -0700

    practice

---
+++ practice.py
@@ -0,0 +1,20 @@
+# strings munpualtion
+# name = " jugdeep"
+# age = "400"
+# print(f'my name is {name} and age is {age}')
+# print("my name is %s and age is %s" %(name,age)) 
+
+from winreg import HKEY_LOCAL_MACHINE
+
+
+k = ['my' , 'jaggi','shah']
+
+print(' ,'.join(k))
+# # split
+# l = " my name is jaggi"
+# print(l.split())
+
+# print(l.partition('m'))
+# # for pADDING rjust,ljust,center
+# r = "hello"
+# print(r.center(20,'*'))
